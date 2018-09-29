837ac6ad1fbba8bb707bbd7b456793178301bc30
David Lee <DavidLee8two7@gmail.com>
2018-09-28 17:10:13 -0700

    Animation to best-sellers section

--- src/Components/Home/BestSellers/BestSellers.js
+++ src/Components/Home/BestSellers/BestSellers.js
@@ -1,79 +1,92 @@
 import React, { Component } from 'react';
 import stripes from '../../../Resources/images/stripes.png';
 import { Tag } from '../../Utils/Misc';
+import Reveal from 'react-reveal/Reveal';
+import Cards from './Cards';
 
 class BestSeller extends Component {
-  state = {};
+  state = {
+    show: false,
+  };
 
   render() {
     return (
-      <div
-        className="best_sellers"
-        style={{ background: `#ffffff url(${stripes}` }}
+      <Reveal
+        fraction={0.7}
+        onReveal={() => {
+          this.setState({ show: true });
+        }}
       >
-        <div className="containers">
-          <div className="best_sellers_wrapper">
-            <div className="home_card_wrapper">Card</div>
-            <div className="home_text_wrapper">
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#ffffff"
-                  custom={{
-                    display: 'inline-block',
-                    marginBottom: '20px',
-                  }}
-                >
-                  Meet
-                </Tag>
+        <div
+          className="best_sellers"
+          style={{ background: `#ffffff url(${stripes}` }}
+        >
+          <div className="containers">
+            <div className="best_sellers_wrapper">
+              <div className="home_card_wrapper">
+                <Cards show={this.state.show} />
               </div>
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#ffffff"
-                  custom={{
-                    display: 'inline-block',
-                    marginBottom: '20px',
-                  }}
-                >
-                  The
-                </Tag>
-              </div>
-              <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#ffffff"
-                  custom={{
-                    display: 'inline-block',
-                    marginBottom: '20px',
-                  }}
-                >
-                  Bests
-                </Tag>
-              </div>
-              <div>
-                <Tag
-                  bck="#ffffff"
-                  size="27px"
-                  color="#0e1731"
-                  link={true}
-                  linkto="/the_team"
-                  custom={{
-                    display: 'inline-block',
-                    marginBottom: '27px',
-                    border: '1px solid #0e1731',
-                  }}
-                >
-                  See more items
-                </Tag>
+              <div className="home_text_wrapper">
+                <div>
+                  <Tag
+                    bck="#0e1731"
+                    size="100px"
+                    color="#ffffff"
+                    custom={{
+                      display: 'inline-block',
+                      marginBottom: '20px',
+                    }}
+                  >
+                    Meet
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#0e1731"
+                    size="100px"
+                    color="#ffffff"
+                    custom={{
+                      display: 'inline-block',
+                      marginBottom: '20px',
+                    }}
+                  >
+                    The
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#0e1731"
+                    size="100px"
+                    color="#ffffff"
+                    custom={{
+                      display: 'inline-block',
+                      marginBottom: '20px',
+                    }}
+                  >
+                    Bests
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    bck="#ffffff"
+                    size="27px"
+                    color="#0e1731"
+                    link={true}
+                    linkto="/the_team"
+                    custom={{
+                      display: 'inline-block',
+                      marginBottom: '27px',
+                      border: '1px solid #0e1731',
+                    }}
+                  >
+                    See more items
+                  </Tag>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Reveal>
     );
   }
 }
