56741704cce0b05d5242b5d387fdc9bf09addbfd
David Lee <DavidLee8two7@gmail.com>
2018-09-28 20:45:55 -0700

    formfields reusable component

--- src/Components/Home/Promotions/index.js
+++ src/Components/Home/Promotions/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PromotionAnimation from './PromotionsAnimation';
+import Enroll from './Enroll';
 
 const Promotions = () => {
   return (
     <div className="promotion_wrapper" style={{ background: '#ffffff' }}>
       <div className="container">
         <PromotionAnimation />
+        <Enroll />
       </div>
     </div>
   );
