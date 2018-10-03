e81bbfea6338f651e8c7a3a8eadde5d4493c2ccb
David Lee <DavidLee8two7@gmail.com>
2018-10-02 23:51:08 -0700

    NodeGroup from react-move for product list

--- src/Components/AllProducts/ProductList.js
+++ src/Components/AllProducts/ProductList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { easePolyOut } from 'd3-ease';
+import NodeGroup from 'react-move/NodeGroup';
 
 class ProductList extends Component {
   state = {
@@ -11,8 +13,53 @@ class ProductList extends Component {
     });
   }
 
+  showProducts = () =>
+    this.state.productList ? (
+      <NodeGroup
+        data={this.state.productList}
+        keyAccessor={d => d.id}
+        start={() => ({
+          opacity: 0,
+          x: -200,
+        })}
+        enter={(d, i) => ({
+          opacity: [1],
+          x: [0],
+          timing: { duration: 800, delay: i * 80, ease: easePolyOut },
+        })}
+        update={(d, i) => ({
+          opacity: [1],
+          x: [0],
+          timing: { duration: 800, delay: i * 80, ease: easePolyOut },
+        })}
+        leave={(d, i) => ({
+          opacity: [1],
+          x: [-200],
+          timing: { duration: 800, delay: i * 80, ease: easePolyOut },
+        })}
+      >
+        {nodes => (
+          <div>
+            {nodes.map(({ key, data, state: { x, opacity } }) => (
+              <div
+                key={key}
+                className="match_box_big"
+                style={{
+                  opacity,
+                  transform: `translate(${x}px)`,
+                }}
+              >
+                inser component here
+              </div>
+            ))}
+          </div>
+        )}
+      </NodeGroup>
+    ) : null;
+
   render() {
-    return <div>List</div>;
+    console.log();
+    return <div>{this.showProducts()}</div>;
   }
 }
 
